a5fcfe6ba4f40e8d1383ae9d8aaf9bc2d0e00c13
Konrad <kszatanek91@gmail.com>
2023-10-30 22:43:25 +0100

    add new functions and refactor old code

--- js/script.js
+++ js/script.js
@@ -29,3 +29,53 @@ document.addEventListener('keydown', (event) => {
     });
   }
 });
+
+
+const fontButton = document.getElementById('fontButton');
+        const fontChangeableElements = document.querySelectorAll('.font-changeable');
+        let fontSizeState = 0; // 0: domyślny rozmiar, 1: większy rozmiar, 2: największy rozmiar
+
+        fontButton.addEventListener('click', () => {
+            fontSizeState = (fontSizeState + 1) % 3;
+
+            switch (fontSizeState) {
+                case 0:
+                    fontChangeableElements.forEach(element => {
+                        element.style.fontSize = '16px';
+                    });
+                    fontButton.textContent = 'Większa czcionka';
+                    break;
+                case 1:
+                    fontChangeableElements.forEach(element => {
+                        element.style.fontSize = '20px';
+                    });
+                    fontButton.textContent = 'Największa czcionka';
+                    break;
+                case 2:
+                    fontChangeableElements.forEach(element => {
+                        element.style.fontSize = '24px';
+                    });
+                    fontButton.textContent = 'Czcionka domyślna';
+                    break;
+            }
+        });
+
+const contrastButton = document.getElementById('contrastButton');
+        let isHighContrast = false;
+
+        contrastButton.addEventListener('click', () => {
+            isHighContrast = !isHighContrast;
+            const contrastChangeableElements = document.querySelectorAll('.contrast-changeable');
+
+            if (isHighContrast) {
+                document.body.classList.add('high-contrast'); /* Dodaj klasę do body, aby zmienić kontrast dla elementów z klasą .contrast-changeable */
+                contrastChangeableElements.forEach(element => {
+                    element.classList.add('high-contrast');
+                });
+            } else {
+                document.body.classList.remove('high-contrast'); /* Usuń klasę, aby przywrócić domyślny kontrast */
+                contrastChangeableElements.forEach(element => {
+                    element.classList.remove('high-contrast');
+                });
+            }
+        });
